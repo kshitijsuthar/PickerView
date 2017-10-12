6a2ddb9a017a18c0be5f06c072aec4a67c5c045e
Kshitij Suthar <kshitijsuthar@kshitijs-macbook-pro.local>
2017-10-11 21:15:08 -0400

    Completed single picker

--- pickers/SingleComponentPickerView.swift
+++ pickers/SingleComponentPickerView.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
-class SingleComponentPickerView: UIViewController {
+class SingleComponentPickerView: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var singlePicker: UIPickerView!
+    
+    private let characterNames = ["Han Solo", "Luke Skywalker", "Leia Skywalker", "Darth Vader", "General Tarkin", "K-2SO"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        
+        let row = singlePicker.selectedRow(inComponent: 0)
+        let selected = characterNames[row]
+        let title = "You selected \(selected)"
+        
+        let alert = UIAlertController(
+            title: title,
+            message: "Thank you for choosing",
+            preferredStyle: .alert)
+        
+        
+        let action = UIAlertAction(
+            title: "You're welcome",
+            style: .default,
+            handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return characterNames.count;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return characterNames[row]
     }
 
 }
